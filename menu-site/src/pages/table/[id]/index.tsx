d7bba7e26bde2cae6c3647373036e6192f52dd0b
brian-ngyn <brian.nguy1en@gmail.com>
2023-10-19 23:39:37 -0600

    table mode!

--- menu-site/src/pages/table/[id]/index.tsx
+++ menu-site/src/pages/table/[id]/index.tsx
@@ -2,13 +2,13 @@ import { useRouter } from "next/router";
 
 import EditContainer from "~/components/Menu/EditContainer";
 
-export default function RestaurantPage() {
+export default function TablePage() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
-      <EditContainer restaurantId={id as string} tableMode={false} />
+      <EditContainer restaurantId={id as string} tableMode={true} />
     </>
   );
 }
