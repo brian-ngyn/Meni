d9833daa5781b5242f71f0fc241ecea6752b2be4
brian-ngyn <brian.nguy1en@gmail.com>
2023-10-23 19:14:32 -0600

    fix navbar logic

--- admin-site/src/components/Navbar/index.tsx
+++ admin-site/src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import router from "next/router";
 
-import { useClerk } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
 import { IconButton } from "@mui/material";
 
 import MeniButton from "~/components/items/MeniButton";
@@ -14,65 +14,12 @@ type NavbarProps = {
 
 export default function Navbar(props: NavbarProps) {
   const { signOut } = useClerk();
+  const { user, isSignedIn, isLoaded } = useUser();
 
-  if (!props.hidden) {
-    return (
-      <div className="sticky top-0 z-40 flex h-20 w-screen items-center border-b-[1px] border-[#353535] bg-backdrop px-4 font-sans md:px-12 lg:px-24">
-        <div className="w-full bg-backdrop">
-          <div className="flex flex-row items-center justify-between">
-            <Link href="/">
-              <IconButton
-                disableRipple
-                style={{ backgroundColor: "transparent" }}
-              >
-                <Image
-                  alt="logo"
-                  src="/landingPage/logo.svg"
-                  width="70"
-                  height="70"
-                />
-              </IconButton>
-            </Link>
-            <div>
-              {props.page === "landingPage" ? (
-                <div className="flex flex-row items-center gap-10">
-                  <Link href="/login">
-                    <div className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
-                      Login
-                    </div>
-                  </Link>
-                  <MeniButton link="/register" rounded normalCase={true}>
-                    Join Now
-                  </MeniButton>
-                </div>
-              ) : props.page === "dashboard" ? (
-                <div className="flex flex-row items-center gap-10">
-                  <Link href="/">
-                    <div className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
-                      Home
-                    </div>
-                  </Link>
-                  <Link href="/">
-                    <div
-                      className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100"
-                      onClick={() => void signOut(() => router.push("/"))}
-                    >
-                      Logout
-                    </div>
-                  </Link>
-                </div>
-              ) : (
-                <div className="flex flex-row items-center gap-10"></div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
+  return (
+    isLoaded && (
       <div className="sticky top-0 z-40 hidden h-16 min-w-full bg-backdrop md:block lg:block">
-        <div className="min-h-full min-w-full border-b-[1px] border-[#353535]   bg-backdrop px-12 py-2 md:px-28">
+        <div className="min-h-full min-w-full border-b-[1px] border-[#353535] bg-backdrop px-12 py-2 md:px-28">
           <div className="flex flex-row items-center justify-between">
             <Link href="/">
               <IconButton
@@ -89,30 +36,44 @@ export default function Navbar(props: NavbarProps) {
             </Link>
             <div>
               {props.page === "landingPage" ? (
-                <div className="flex flex-row items-center gap-10">
-                  <Link href="/login">
-                    <div className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
-                      Login
+                <>
+                  {user && isSignedIn ? (
+                    <div className="flex flex-row items-center gap-10">
+                      <MeniButton link="/dashboard" rounded>
+                        Dashboard
+                      </MeniButton>
                     </div>
-                  </Link>
-                  <MeniButton link="/register" rounded>
-                    Join Now
-                  </MeniButton>
-                </div>
+                  ) : (
+                    <div className="flex flex-row items-center gap-10">
+                      <Link href="/login">
+                        <div className="relative font-sans text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
+                          Login
+                        </div>
+                      </Link>
+                      <MeniButton link="/register" rounded>
+                        Join Now
+                      </MeniButton>
+                    </div>
+                  )}
+                </>
               ) : props.page === "dashboard" ? (
                 <div className="flex flex-row items-center gap-10">
                   <Link href="/">
-                    <div className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
+                    <div className="relative font-sans text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100">
                       Home
                     </div>
                   </Link>
                   <Link href="/">
-                    <div
-                      className="relative text-lg before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-[#545454] before:transition-transform before:duration-500 hover:before:origin-left hover:before:scale-x-100"
-                      onClick={() => void signOut(() => router.push("/"))}
+                    <MeniButton
+                      onClick={() =>
+                        void signOut(
+                          () => void router.push("/"), // need both for some reason...
+                        )
+                      }
+                      rounded
                     >
                       Logout
-                    </div>
+                    </MeniButton>
                   </Link>
                 </div>
               ) : (
@@ -122,8 +83,8 @@ export default function Navbar(props: NavbarProps) {
           </div>
         </div>
       </div>
-    );
-  }
+    )
+  );
 }
 
 //TODO: Add conditional rendering for logged in users.
