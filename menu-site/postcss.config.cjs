c465a0276b96de1fea622f379584741343cd10f7
brian-ngyn <brian.nguy1en@gmail.com>
2023-10-30 14:09:51 -0600

    try obfuscate

--- menu-site/postcss.config.cjs
+++ menu-site/postcss.config.cjs
@@ -1,7 +1,17 @@
+const isObfscMode = process.env.NODE_ENV !== "development";
+
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-obfuscator": {
+      enable: isObfscMode,
+      extensions: [".tsx"],
+      formatJson: true,
+      callBack: function () {
+        process.env.NODE_ENV = "production"; // to make sure postcss-obfuscator doesn't re-run.
+      },
+    },
   },
 };
 
