213e62711cc85cee2bb50f27a4410afe5f73ff10
brian-ngyn <brian.nguy1en@gmail.com>
2023-11-03 15:46:57 -0600

    remove css obfuscate

--- menu-site/postcss.config.cjs
+++ menu-site/postcss.config.cjs
@@ -1,17 +1,7 @@
-const isObfscMode = process.env.NODE_ENV !== "development";
-
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    "postcss-obfuscator": {
-      enable: isObfscMode,
-      extensions: [".tsx"],
-      formatJson: true,
-      callBack: function () {
-        process.env.NODE_ENV = "production"; // to make sure postcss-obfuscator doesn't re-run.
-      },
-    },
   },
 };
 
